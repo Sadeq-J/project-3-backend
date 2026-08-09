f536cecb1ae943364be5051501eec0befb36bb4e
Sadiq-J <sadiq_b2@hotmail.com>
2026-08-09 09:26:07 +0300

    feat: create Booking Schema

---
+++ models/Booking.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+
+// Schema
+
+const bookingSchema = new mongoose.Schema({
+    venue:{
+        type : mongoose.Schema.Types.ObjectId,
+        ref: "Venue",
+        required: true
+    },
+    owner:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    date:{
+        type: Date,
+        required: true
+    },
+    timeSlots:{
+        type: String,
+        required: true
+    },
+    status:{
+        type: String,
+        enum: ['Confirmed', 'Cancelled'],
+        default: 'Confirmed'
+    },
+    invitedPlayers:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    teams:{
+        teamA:[{
+            type: String
+        }],
+        teamB:[{
+            type: String
+        }],
+    }
+}, {timestamps: true})
+
+
+
+// model
+const Booking = mongoose.model('Booking', bookingSchema)
+
+
+
+module.exports = Booking
