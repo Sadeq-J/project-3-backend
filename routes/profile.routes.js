5b92df6a4a80572592f833331a1bd1a07bea3c78
Sadiq-J <sadiq_b2@hotmail.com>
2026-08-09 16:21:29 +0300

    fix

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { getProfile, getUserProfile, followUser, unfollowUser, updateProfile} = require("../controllers/profile.controller");
 const { veverifyToken } = require("../middleware/verifyToken");
 
-router.get("/myProfile", verifyToken, getProfile);
+router.get("/me", verifyToken, getProfile);
 router.get("/:userId", getUserProfile);
 router.post("/follow", verifyToken, followUser);
 router.post("/unfollow", verifyToken, unfollowUser);
