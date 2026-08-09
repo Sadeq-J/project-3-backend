d8daf9d80f1b2cfc6c081f7c5e62ca798f26cae9
Sadiq-J <sadiq_b2@hotmail.com>
2026-08-09 16:15:46 +0300

    feat: create profile controller

--- models/User.js
+++ models/User.js
@@ -24,7 +24,11 @@ const userSchema = new mongoose.Schema(
     following: [{
       type: mongoose.Schema.Types.ObjectId,
       ref: "User"
-    }]
+    }],
+    profilePicture: {
+      type: String,
+      default: "https://res.cloudinary.com/dxjv7gq3f/image/upload/v1697040910/default-profile-picture-1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_1_q6kz8b.png"
+    }
   },
   { timestamps: true },
 );
