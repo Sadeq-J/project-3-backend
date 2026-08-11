a28543de0d58d5667681579afd3f16017203ab26
Sadeq Ali <43050822+Sadeq-J@users.noreply.github.com>
2026-08-11 09:40:57 +0300

    Add follower and following routes to profile

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,15 +1,17 @@
 const router = require('express').Router();
-const { getProfile, getUserProfile, followUser, unfollowUser, updateProfile, getfriends, searchFriends} = require('../controllers/profile.controller');
+const { getProfile, getUserProfile, followUser, unfollowUser, updateProfile, getfriends, searchFriends, getMyFollowers, getMyFollowing} = require('../controllers/profile.controller');
 const verifyToken = require('../middleware/verifyToken');
 const upload = require('../middleware/upload'); // Cloudinary/Multer middleware
 
+router.get('/followers', verifyToken, getMyFollowers)
+router.get('/following', verifyToken, getMyFollowing)
 router.get('/me', verifyToken, getProfile);
 router.put('/me', verifyToken, upload.single('profilePicture'), updateProfile);
 router.get('/friends', verifyToken, getfriends);
 router.get('/friends/search', verifyToken, searchFriends)
 router.get('/:id', verifyToken, getUserProfile);
-router.post('/follow', verifyToken, followUser);
-router.post('/unfollow', verifyToken, unfollowUser);
+router.post('/:id/follow', verifyToken, followUser)
+router.post('/:id/unfollow', verifyToken, unfollowUser)
 
 
 module.exports = router;
