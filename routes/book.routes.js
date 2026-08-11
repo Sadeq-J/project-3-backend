2ed7a03185d0e829d71f132e3f60ee21cd74e0e2
ALIALSAEED313 <202300573@student.polytechnic.bh>
2026-08-11 09:39:40 +0300

    Created the invitation System

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {createBooking, getBooking, updateBooking} = require('../controllers/booking.controller')
+const {createBooking, getBooking, updateBooking, invitePlayer, getMyInnvitations, joinBookingTeam} = require('../controllers/booking.controller')
 const verifyToken = require('../middleware/verifyToken')
 
 
@@ -8,6 +8,9 @@ const verifyToken = require('../middleware/verifyToken')
 router.post('/:id', verifyToken ,createBooking)
 router.get('/my-booking', getBooking)
 router.put('/:id/edit', updateBooking)
+router.post('/:id/invite', verifyToken, invitePlayer)
+router.get('/invitations/me', verifyToken, getMyInnvitations)
+router.post('/:id/join', verifyToken, joinBookingTeam)
 
 
 module.exports = router
