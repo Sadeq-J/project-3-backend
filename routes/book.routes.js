5db5f32dcd8833600e6e3f37759c15cdc6b97cae
Sadiq-J <sadiq_b2@hotmail.com>
2026-08-12 12:40:14 +0300

    fix

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
-const {createBooking, getBooking, updateBooking, invitePlayer, getMyInnvitations, joinBookingTeam} = require('../controllers/booking.controller')
+const {createBooking, getBooking, updateBooking, invitePlayer, getMyInnvitations, joinBookingTeam, getBookingsByVenue} = require('../controllers/booking.controller')
 const verifyToken = require('../middleware/verifyToken')
 
 
 
 
 router.post('/:id', verifyToken ,createBooking)
+router.get('/venue/:id', getBookingsByVenue)
 router.get('/my-booking', verifyToken, getBooking)
 router.put('/:id/edit', verifyToken, updateBooking)
 router.post('/:id/invite', verifyToken, invitePlayer)
