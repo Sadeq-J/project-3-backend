0d739e80645c931b8c194196bc7f8aa235c0209b
Sadeq Ali <43050822+Sadeq-J@users.noreply.github.com>
2026-08-12 07:19:06 +0300

    Add routes for fetching bookings by venue and all bookings

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
-const {createBooking, getBooking, updateBooking, invitePlayer, getMyInnvitations, joinBookingTeam} = require('../controllers/booking.controller')
+const {createBooking, getBooking, updateBooking, invitePlayer, getMyInnvitations, joinBookingTeam, getBookingsByVenue, getAllBookings} = require('../controllers/booking.controller')
 const verifyToken = require('../middleware/verifyToken')
 
 
 
-
+router.get('/', verifyToken, getAllBookings)
 router.post('/:id', verifyToken ,createBooking)
+router.get("/venue/:id", verifyToken, getBookingsByVenue)
 router.get('/my-booking', getBooking)
 router.put('/:id/edit', updateBooking)
 router.post('/:id/invite', verifyToken, invitePlayer)
