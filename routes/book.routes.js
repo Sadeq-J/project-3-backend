c5651e5dd8f8b4ae87375d964e2cf75f4bcfb503
Sadiq-J <sadiq_b2@hotmail.com>
2026-08-12 12:00:34 +0300

    fix: book routes

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -6,8 +6,8 @@ const verifyToken = require('../middleware/verifyToken')
 
 
 router.post('/:id', verifyToken ,createBooking)
-router.get('/my-booking', getBooking)
-router.put('/:id/edit', updateBooking)
+router.get('/my-booking', verifyToken, getBooking)
+router.put('/:id/edit', verifyToken, updateBooking)
 router.post('/:id/invite', verifyToken, invitePlayer)
 router.get('/invitations/me', verifyToken, getMyInnvitations)
 router.post('/:id/join', verifyToken, joinBookingTeam)
