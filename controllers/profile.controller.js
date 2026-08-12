93a18171a83ad47f77165748a3b548ad9d6f5f34
Sadiq-J <sadiq_b2@hotmail.com>
2026-08-12 09:55:41 +0300

    fix: follow user function

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -95,7 +95,7 @@ const followUser = async (req, res) => {
     await targetUser.save()
 
     await Notification.create({
-      recipient: targetUserId,
+      recipient: id,
       sender: userId,
       type: 'follow'
     })
